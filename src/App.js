08ca8bea3ee201cc77937940c5c31670cfbbefcc
jmlinares2019 <jlinaresmar@gmail.com>
2024-02-13 19:57:42 +0100

    - Calling Api on button click and passing data to component via props - Managing data availabilty with states

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import imgRickMorty from "./img/rick-morty.png";
 import './App.css';
+import { useState } from "react";
+import Characters from "./components/Characters";
 
 function App() {
-  const reqApi = () => {
-    console.log("Clicked!");
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+
+    setCharacters(characterApi.results);
+
+    console.log(characters);
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
-        <img src={imgRickMorty} alt="Rick & Morty" className='img-home' />
-        <button onClick={reqApi} className='btn-search'>Search characters</button>
+        {characters ? // does characters contain data? 
+          <Characters characters={characters}/> // load Characters component passing characters as prop
+        : // else  
+          < /* load image and button into fragment (empty HTML brackets) */ >
+            <img src={imgRickMorty} alt="Rick & Morty" className='img-home' />
+            <button onClick={reqApi} className='btn-search'>Search characters</button>
+          </>
+        } 
       </header>
     </div>
   );
